eb0bbef108cc4348fd8f54d230efb5b8e4116a91
yuanyahui1 <2423111384@qq.com>
2019-06-09 20:33:22 +0800

    第三次改动 创建按钮 2019年6月9日19:43:11

--- app/src/main/java/com/bawei/git_demo/MainActivity.java
+++ app/src/main/java/com/bawei/git_demo/MainActivity.java
@@ -2,6 +2,8 @@ package com.bawei.git_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +15,8 @@ public class MainActivity extends AppCompatActivity {
         
         //注释：演示用
         TextView tv_Name = new TextView(this, null);
+        //输入框
+        EditText editText = new EditText(this, null);
+        Button button = new Button(this, null);
     }
 }
